c4084473d3619c4c18d224ab79902029dd06d367
Amir Alimzhanov <aalimzhanov@evolution.com>
2023-03-25 22:08:15 +0400

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ApplicationModule } from './application.module';
 
 async function bootstrap() {
   const application = await NestFactory.create(ApplicationModule);
+  application.enableCors();
   await application.listen(3000);
 }
 bootstrap();
