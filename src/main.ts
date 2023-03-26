ed00a8e9d428f612b9a9808de5e228161132f84d
Amir Alimzhanov <aalimzhanov@evolution.com>
2023-03-27 02:00:31 +0400

    Set global guard & add Public decorator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { ApplicationModule } from './application.module';
+import { JwtAuthGuard } from './modules/auth/auth.jwt.guard';
 
 async function bootstrap() {
   const application = await NestFactory.create(ApplicationModule);
   application.enableCors();
+  const reflector = application.get(Reflector)
+  application.useGlobalGuards(new JwtAuthGuard(reflector));
   await application.listen(3000);
 }
 bootstrap();
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,25 +1,19 @@
-import { Body, Controller, Get, HttpStatus, Param, Post, Put, Res, UseGuards } from "@nestjs/common";
-import { JwtAuthGuard } from "../auth/auth.jwt.guard";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Public } from "src/decorators/public.decorator";
 import { UserDto } from "./user.dto";
 import { UserType, UserService } from "./user.service";
 
 
 
 @Controller()
-// @UseGuards(JwtAuthGuard)
 export class UserController {
     constructor(private readonly UserService: UserService) {}
 
     @Post('user')
+    @Public()
     createUser(@Body() body: UserDto): Promise<UserType> {
         return this.UserService.createUser(body);
     }
-
-    // @Put('user')
-    // changeUserPassword(@Res() res ,@Body() body: UserDto): void {
-    //     this.UserService.changePassword(body);
-    //     res.status(HttpStatus.OK).send();
-    // }
 
     @Get('user/:id')
     getUser(@Param() { id }): Promise<UserType | undefined> {
