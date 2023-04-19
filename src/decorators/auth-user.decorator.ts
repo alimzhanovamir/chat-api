b5ea843652fd5bbad593a466d49cb357ee37a50e
Amir Alimzhanov <aalimzhanov@evolution.com>
2023-04-19 04:20:26 +0400

    Fix room remove functionality

--- src/decorators/auth-user.decorator.ts
+++ src/decorators/auth-user.decorator.ts
@@ -2,13 +2,12 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { JwtAuthStrategy } from "src/modules/auth/auth.jwt.strategy";
 
 export interface AuthRequest extends Request {
-    email: Awaited<ReturnType<JwtAuthStrategy["validate"]>>;
+    user: Awaited<ReturnType<JwtAuthStrategy["validate"]>>;
 }
 
 export const AuthUser = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
-        const req = ctx.switchToHttp().getRequest<AuthRequest>();
-
-        return req.email;
+        const { user } = ctx.switchToHttp().getRequest<AuthRequest>();
+        return user;
     },
 );
